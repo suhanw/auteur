1a1c9997039bce1fc1536f7db13093ad35e2a10c
Suhan Wijaya <suhanw@gmail.com>
2018-11-09 18:22:34 -0500

    update carousel

--- src/client/components/carousel.jsx
+++ src/client/components/carousel.jsx
@@ -75,18 +75,19 @@ class Carousel extends React.Component {
       }
       const timeElapsed = currTime - that.firstTouch;
       if (timeElapsed > delay ) { // only call handler when wheeling has occured for specified delay
-        // console.log(timeElapsed, e.deltaY);
+        console.log(timeElapsed, e.deltaY);
         const scrollDir = e.deltaY > 0 ? 'up' : 'down';
         handleWheel(scrollDir);
         that.firstTouch = null;
       }
-      // console.log(that.firstTouch);
+      console.log(that.firstTouch);
     };
   }
 
   scrollCarousel(scrollDir, nextSlide) {
     const that = this;
     const {activeSlide, slideClasses} = that.state;
+    console.log('curr active slide', activeSlide);
     let newActiveSlide = nextSlide;
     let newSlideClasses = merge({}, slideClasses);
     // if user is wheeling up AND not at last slide
@@ -108,6 +109,7 @@ class Carousel extends React.Component {
       that.firstTouch = null
       return;
     }
+    console.log('new active slide', newActiveSlide);
     that.setState({
       activeSlide: newActiveSlide,
       slideClasses: newSlideClasses
