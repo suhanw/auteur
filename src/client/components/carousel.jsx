3a9d22ffb327c7de93ebdc700c79cc2b1119f3f0
Suhan Wijaya <suhanw@gmail.com>
2018-11-09 11:45:32 -0500

    add Carousel slidedown effect

--- src/client/components/carousel.jsx
+++ src/client/components/carousel.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Route} from 'react-router-dom'
+import {merge} from 'lodash';
 import {AuthRoute} from '../util/route_util';
 import {throttleWheelEvent} from '../util/carousel_util';
 import SessionFormContainer from './session/session_form_container';
@@ -10,11 +11,11 @@ class Carousel extends React.Component {
 
     this.state = {
       activeSlide: 1,
-      slides: {
-        'intro-slide': 1,
-        'about-slide': 2,
-        'create-slide': 3,
-        'welcome-slide': 4,
+      slideClasses: {
+        1: 'intro-slide active',
+        2: 'about-slide',
+        3: 'create-slide',
+        4: 'welcome-slide',
       },
     }
 
@@ -23,26 +24,26 @@ class Carousel extends React.Component {
   }
 
   render() {
-    const {activeSlide, slides} = this.state;
+    const {activeSlide, slideClasses} = this.state;
     console.log('current active slide', activeSlide);
 
     return(
       <section className='carousel'>
-        <div  className={slides['welcome-slide'] === activeSlide ? 'welcome-slide active' : 'welcome-slide'}
+        <div  className={slideClasses[4]}
               onWheel={this.throttleWheel(200, this.scrollCarousel)}>
           This is welcome slide.
           <AuthRoute path='/login' component={SessionFormContainer} />
           <AuthRoute path='/signup' component={SessionFormContainer} />
         </div>
-        <div  className={slides['create-slide'] === activeSlide ? 'create-slide active' : 'create-slide'}
+        <div  className={slideClasses[3]}
               onWheel={this.throttleWheel(200, this.scrollCarousel)}>
           This is create slide.
         </div>
-        <div  className={slides['about-slide'] === activeSlide ? 'about-slide active' : 'about-slide'}
+        <div  className={slideClasses[2]}
               onWheel={this.throttleWheel(200, this.scrollCarousel)}>
           This is about slide.
         </div>
-        <div  className={slides['intro-slide'] === activeSlide ? 'intro-slide active' : 'intro-slide'}
+        <div  className={slideClasses[1]}
               onWheel={this.throttleWheel(200, this.scrollCarousel)}>
           <h1 className='logo'>auteur</h1>
           <AuthRoute path='/login' component={SessionFormContainer} />
@@ -77,23 +78,31 @@ class Carousel extends React.Component {
 
   scrollCarousel(e, currTime, delay) {
     const that = this;
-    const {activeSlide, slides} = that.state;
-    let newActiveSlide = activeSlide;
+    const {activeSlide, slideClasses} = that.state;
+    let newActiveSlide;
+    let newSlideClasses = merge({}, slideClasses);
     // if user is wheeling up AND not at last slide
-    if (e.deltaY > 0 && activeSlide < (Object.keys(slides).length)) {
+    if (e.deltaY > 0 && activeSlide < (Object.keys(slideClasses).length)) {
       // scroll to next slide
-      newActiveSlide += 1;
+      newSlideClasses[activeSlide] = newSlideClasses[activeSlide].replace(' active', '');
+      newActiveSlide = activeSlide + 1;
+      newSlideClasses[newActiveSlide] = newSlideClasses[newActiveSlide] + ' active';
     }
     // else if user is wheeling down AND not at first slide
     else if (e.deltaY < 0 && activeSlide > 1) {
       // scroll to previous slide
-      newActiveSlide -= 1;
+      newSlideClasses[activeSlide] = newSlideClasses[activeSlide].replace(' active', '');
+      newActiveSlide = activeSlide -1;
+      newSlideClasses[newActiveSlide] = newSlideClasses[newActiveSlide] + ' active slidedown';
     } else { // when wheeling past first or last slide, don't re-render
       that.firstTouch = null
       return;
     }
     console.log('new activeSlide', newActiveSlide);
-    that.setState({activeSlide: newActiveSlide});
+    that.setState({
+      activeSlide: newActiveSlide,
+      slideClasses: newSlideClasses
+    });
     that.firstTouch = null;
   }
 
