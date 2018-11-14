a987af61741fa14a8599d379cf222f0d57d51007
Suhan Wijaya <suhanw@gmail.com>
2018-11-13 19:01:05 -0500

    add SHOW blog route

--- src/server/routes/blogs.js
+++ src/server/routes/blogs.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const Blog = require('../models/blog');
 
-// GET api/blog/:id
+// GET api/blog/:id - SHOW blog
 router.get('/blogs/:id', function (req, res) {
-    Blog.findById(req.params.id)
-        .populate('posts')
+    Blog.findOne({ _id: req.params.id })
+        .populate({
+            path: 'author',
+            select: '_id username avatarImageUrl',
+        })
         .exec(function (err, foundBlog) {
             if (err) {
                 return res.json([err.message]);
