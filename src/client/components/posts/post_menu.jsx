4ee6faddde51c0a31ce0100c9415bfbbcca8254f
Suhan Wijaya <suhanw@gmail.com>
2018-11-16 08:09:40 -0500

    style PostMenu component

--- src/client/components/posts/post_menu.jsx
+++ src/client/components/posts/post_menu.jsx
@@ -8,12 +8,30 @@ class PostMenu extends React.Component {
   render() {
     return (
       <ul className='post-menu'>
-        <li className='post-menu-item'><i className="fas fa-font"></i></li>
-        <li className='post-menu-item'><i className="fas fa-camera-retro"></i></li>
-        <li className='post-menu-item'><i className="fas fa-quote-left"></i></li>
-        <li className='post-menu-item'><i className="fas fa-link"></i></li>
-        <li className='post-menu-item'><i className="fas fa-headphones-alt"></i></li>
-        <li className='post-menu-item'><i className="fas fa-video"></i></li>
+        <li className='post-menu-item'>
+          <i className="fas fa-font"></i>
+          <span>Text</span>
+        </li>
+        <li className='post-menu-item'>
+          <i className="fas fa-camera-retro"></i>
+          <span>Photo</span>
+        </li>
+        <li className='post-menu-item'>
+          <i className="fas fa-quote-left"></i>
+          <span>Quote</span>
+        </li>
+        <li className='post-menu-item'>
+          <i className="fas fa-link"></i>
+          <span>Link</span>
+        </li>
+        <li className='post-menu-item'>
+          <i className="fas fa-headphones-alt"></i>
+          <span>Audio</span>
+        </li>
+        <li className='post-menu-item'>
+          <i className="fas fa-video"></i>
+          <span>Video</span>
+        </li>
       </ul>
     )
   }
