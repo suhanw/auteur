6aba505e2da8d13bdd93168cdb701dffbff2493e
Suhan Wijaya <suhanw@gmail.com>
2018-11-19 09:45:25 -0500

    refactor promise chaining

--- src/server/routes/blogs.js
+++ src/server/routes/blogs.js
@@ -18,55 +18,54 @@ router.get('/blogs/:id', function (req, res) {
 
 // GET api/blogs/:id/posts - INDEX blog posts (should be moved to 'posts.js' routes)
 router.get('/blogs/:id/posts', function (req, res) {
-    Blog.findOne({ _id: req.params.id })
-        .exec(function (err, foundBlog) {
-            if (err || !foundBlog) {
-                return res.status(404).json(['The blog does not exist.']);
-            }
+    modelQuery.findOneBlog(
+        req.params.id,
+        (foundBlog) => {
             Post.find({ blog: foundBlog._id })
                 .sort({ 'createdAt': 'desc' })
                 .select('type title media body likeCount commentCount blog createdAt')
-                .exec(function (err, posts) {
-                    if (err) return res.json([err.message]);
-                    return res.json(posts);
-                });
-        });
+                .exec()
+                .then((posts) => res.json(posts))
+                .catch((err) => res.json([err.message]));
+        },
+        (err) => res.status(404).json(['The blog does not exist.']), // failure callback
+    );
 });
 
 // POST api/blogs/:id/posts
 router.post('/blogs/:id/posts', function (req, res) {
-    Blog.findOne({ _id: req.params.id })
-        .exec(function (err, foundBlog) {
-            if (err || !foundBlog) {
-                return res.status(404).json(['The blog does not exist.']);
-            }
+    modelQuery.findOneBlog(
+        req.params.id,
+        (foundBlog) => {
             let newPost = lodash.merge({}, req.body);
             newPost.body = sanitizeHtml(newPost.body);
-            Post.create(newPost, function (err, createdPost) {
-                if (err) return res.status(422).json([err.message]);
-                // remember to increment postCount in blog model
-                foundBlog.postCount += 1;
-                foundBlog.save();
-                return res.json(createdPost);
-            });
-        });
+            Post.create(newPost)
+                .then((createdPost) => {
+                    foundBlog.postCount += 1;
+                    foundBlog.save();
+                    return res.json(createdPost);
+                })
+                .catch((err) => res.status(422).json([err.message]))
+        },
+        (err) => res.status(404).json(['The blog does not exist.']), // failure callback
+    );
 });
 
 // DELETE api/blogs/:id/posts/:id
 router.delete('/blogs/:id/posts/:postId', function (req, res) {
     // FIX: need to add middleware to authenticate and authorize user!
-    Blog.findOne({ _id: req.params.id })
-        .exec(function (err, foundBlog) {
-            if (err || !foundBlog) {
-                return res.status(404).json(['The blog does not exist.']);
-            }
+    modelQuery.findOneBlog(
+        req.params.id,
+        (foundBlog) => {
             Post.deleteOne({ _id: req.params.postId }, function (err) {
                 if (err) return res.status(422).json([err.message]);
                 foundBlog.postCount -= 1;
                 foundBlog.save();
                 return res.json(req.params.postId);
             });
-        });
+        },
+        (err) => res.status(404).json(['The blog does not exist.']), // failure callback
+    );
 });
 
 // PUT api/blogs/:id/posts/:id
