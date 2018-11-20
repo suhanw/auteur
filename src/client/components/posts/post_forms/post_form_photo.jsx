7fcbec2b210ea2380f5e8efa3ad9c406a7e27aae
Suhan Wijaya <suhanw@gmail.com>
2018-11-20 10:21:53 -0500

    add an image preview for PostFormPhoto

--- src/client/components/posts/post_forms/post_form_photo.jsx
+++ src/client/components/posts/post_forms/post_form_photo.jsx
@@ -12,32 +12,42 @@ class PostFormPhoto extends React.Component {
     // populate state for edit form (i.e., post is not null), otherwise state is blank for new form
     this.state = props.post ? props.post : {
       type: 'photo',
-      title: '',
       body: '',
+      media: null,
+      mediaPreviewURL: '',
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleMediaFile = this.handleMediaFile.bind(this);
   }
 
   render() {
     const { blog, closePostForm } = this.props;
-    const { title, body } = this.state;
+    // const { title, body } = this.state;
+    const { mediaPreviewURL } = this.state;
+    // to show preview when user selects file
+    const mediaPreview = mediaPreviewURL ? <img src={mediaPreviewURL} className='post-photo' /> : null;
     return (
       <form className='post-form' onSubmit={this.handleSubmit}>
 
         <PostFormHeader blog={blog} />
 
-        <fieldset className='post-main'>
+        <main className='post-main'>
+          {mediaPreview}
           <div className='media-upload'>
             <span className='photo-upload'>
-              <input type='file' accept='image/*' />
+              <input
+                type='file'
+                accept='image/*'
+                name='photo'
+                onChange={this.handleMediaFile} />
             </span>
             <span className='photo-link'>
               Add photos from web
             </span>
           </div>
-        </fieldset>
+        </main>
 
         <PostFormFooter closePostForm={closePostForm} />
 
@@ -54,18 +64,39 @@ class PostFormPhoto extends React.Component {
     };
   }
 
+  handleMediaFile(e) {
+    const mediaFile = e.target.files[0];
+    const fileReader = new FileReader();
+    if (mediaFile) {
+      fileReader.readAsDataURL(mediaFile); // to get URL for media preview
+    }
+    console.log(fileReader.result);
+    const that = this;
+    fileReader.addEventListener('load', function () { // readAsDataURL is async, so set state when result returned.
+      // FIX: add a loader for preview image
+      console.log(fileReader.result);
+      that.setState(
+        { media: mediaFile, mediaPreviewURL: fileReader.result },
+        () => console.log(that.state)
+      );
+    })
+
+  }
+
   handleSubmit(e) {
     e.preventDefault();
     e.persist(); // to prevent React synthetic event from nullified, and be able to be passed into closePostForm
     // create newPost obj (get blog._id from props)
     const { currentUser, blog, submitAction, closePostForm } = this.props;
-    let newPost = merge({}, this.state);
-    newPost.author = currentUser._id;
-    newPost.blog = blog._id;
+    let newPost = new FormData(this.state);
+    newPost.append('author', currentUser._id);
+    newPost.append('blog', blog._id);
+    newPost.append('body', this.state.body);
+    newPost.append('media', this.state.media);
     // invoke AJAX to create new post or edit post
-    submitAction(newPost).then(
-      () => closePostForm(e) // close form after posting
-    );
+    // submitAction(newPost).then(
+    //   () => closePostForm(e) // close form after posting
+    // );
   }
 }
 
