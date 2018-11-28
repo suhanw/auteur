e3390164c173737c972272acf15498781278d33d
Suhan Wijaya <suhanw@gmail.com>
2018-11-28 13:24:12 -0500

    cleanup

--- src/server/routes/posts.js
+++ src/server/routes/posts.js
@@ -7,18 +7,6 @@ const sanitizeHtml = require('sanitize-html');
 const lodash = require('lodash');
 const mediaUpload = require('../util/media_upload_util');
 
-// SET UP AWS FILE UPLOAD=====================
-const AWS = require('aws-sdk');
-
-let s3bucket = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
-  Bucket: process.env.AWS_BUCKET,
-});
-// SET UP AWS FILE UPLOAD=====================
-
-
 const Blog = require('../models/blog');
 const Post = require('../models/post');
 const modelQuery = require('../util/model_query_util');
