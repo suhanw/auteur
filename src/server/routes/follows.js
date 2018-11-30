c520b1353a755cbc47ad8a34d90e66eb4fb69149
Suhan Wijaya <suhanw@gmail.com>
2018-11-29 19:30:12 -0500

    cleanup

--- src/server/routes/follows.js
+++ src/server/routes/follows.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const lodash = require('lodash');
+
 const modelQuery = require('../util/model_query_util');
 const middleware = require('../middleware/middleware');
 const User = require('../models/user');
