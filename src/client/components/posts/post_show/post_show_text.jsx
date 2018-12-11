c65e4c96cb41f30a981cafdaf745a884275be275
Suhan Wijaya <suhanw@gmail.com>
2018-12-10 19:14:27 -0500

    fix post show stylings

--- src/client/components/posts/post_show/post_show_text.jsx
+++ src/client/components/posts/post_show/post_show_text.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 class PostShowText extends React.Component {
   constructor(props) {
     super(props);
+
+    this.renderBody = this.renderBody.bind(this);
   }
 
   render() {
@@ -10,12 +12,21 @@ class PostShowText extends React.Component {
     return (
       <main className='post-main'>
         <h1 className='post-title'>{post.title}</h1>
-        <div className='post-body'
-          dangerouslySetInnerHTML={{ __html: post.body }}>
-        </div>
+        {this.renderBody()}
       </main >
     )
   }
+
+  renderBody() {
+    const { post } = this.props;
+    if (!post.body) return null;
+    return (
+      <div className='post-body'
+        dangerouslySetInnerHTML={{ __html: post.body }}>
+      </div>
+    )
+  }
+
 }
 
 export default PostShowText;
