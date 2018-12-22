eaea16077d50a0de9e365c24748c56f9be22093d
Suhan Wijaya <suhanw@gmail.com>
2018-12-22 16:21:36 -0500

    add GlobalContextProvider to Root

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -10,35 +10,40 @@ import Drawer from './drawers/drawer';
 import { ErrorPage } from './errors/errors';
 import { AuthRoute, ProtectRoute } from '../util/route_util';
 import NavbarContainer from './navbar/navbar_container';
-import GlobalContextProvider from './global_ context_provider';
+import { GlobalContext } from './global_ context_provider';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.isLoggedIn = this.isLoggedIn.bind(this);
   }
 
   render() {
-
     return (
       <div>
-        <GlobalContextProvider>
-          <Modal />
-          <Drawer />
-          <NavbarContainer />
-          <Switch>
-            <ProtectRoute path='/search/:query' component={SearchPage} />
-            <ProtectRoute path='/settings' component={Settings} />
-            <ProtectRoute path='/dashboard' component={Dashboard} />
-            <AuthRoute path='/login' component={Carousel} />
-            <AuthRoute path='/signup' component={Carousel} />
-            <AuthRoute exact path='/' component={Carousel} />
-            <Route component={ErrorPage} />
-          </Switch>
-          <ChatDrawer />
-        </GlobalContextProvider>
+        <Modal />
+        <Drawer />
+        <NavbarContainer />
+        <Switch>
+          <ProtectRoute path='/search/:query' component={SearchPage} />
+          <ProtectRoute path='/settings' component={Settings} />
+          <ProtectRoute path='/dashboard' component={Dashboard} />
+          <AuthRoute path='/login' component={Carousel} />
+          <AuthRoute path='/signup' component={Carousel} />
+          <AuthRoute exact path='/' component={Carousel} />
+          <Route component={ErrorPage} />
+        </Switch>
+        {this.isLoggedIn() ? <ChatDrawer /> : null}
       </div>
     );
   }
+
+  isLoggedIn() {
+    return this.context.currentUser;
+  }
 }
+
+App.contextType = GlobalContext;
 
 export default App;
--- src/client/components/root.jsx
+++ src/client/components/root.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './app';
-
+import GlobalContextProvider from './global_ context_provider';
 
 const Root = function (props) {
   const { store } = props;
   return (
     <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <GlobalContextProvider>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </GlobalContextProvider>
     </Provider>
   );
 };
