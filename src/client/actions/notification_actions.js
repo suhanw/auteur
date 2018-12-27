358f68bc806375b9da63a39cfddb2465dafa5df3
Suhan Wijaya <suhanw@gmail.com>
2018-12-27 17:10:44 -0500

    add actions to fetch notifications

--- src/client/actions/notification_actions.js
+++ src/client/actions/notification_actions.js
@@ -1,4 +1,8 @@
+import * as APIUtil from '../util/api_util/notification_api_util';
+
 export const RECEIVE_NOTIFICATIONS = 'RECEIVE_NOTIFICATIONS';
+export const RECEIVE_UNREAD_NOTIFICATION_COUNT = 'RECEIVE_UNREAD_NOTIFICATION_COUNT';
+export const RECEIVE_NOTIFICATION_ERRORS = 'RECEIVE_NOTIFICATION_ERRORS';
 
 export const receiveNotifications = function (notifications) {
   return {
@@ -7,3 +11,34 @@ export const receiveNotifications = function (notifications) {
   };
 };
 
+export const receiveUnreadNotificationCount = function (unreadCount) {
+  return {
+    type: RECEIVE_UNREAD_NOTIFICATION_COUNT,
+    payload: unreadCount,
+  };
+};
+
+export const receiveNotificationErrors = function (errors) {
+  return {
+    type: RECEIVE_NOTIFICATION_ERRORS,
+    payload: errors,
+  };
+};
+
+export const fetchNotifications = function () {
+  return function (dispatch) {
+    return APIUtil.fetchNotifications().then(
+      (notifications) => dispatch(receiveNotifications(notifications)),
+      (err) => dispatch(receiveNotificationErrors(err.responseJSON)),
+    );
+  };
+};
+
+export const fetchUnreadNotificationCount = function () {
+  return function (dispatch) {
+    return APIUtil.fetchNotifications({ countUnread: true }).then(
+      (unreadCount) => dispatch(receiveUnreadNotificationCount(unreadCount)),
+      (err) => dispatch(receiveNotificationErrors(err.responseJSON)),
+    );
+  };
+};
