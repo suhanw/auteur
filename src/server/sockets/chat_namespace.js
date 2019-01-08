5393c10c995db5e6e229997b7a92b5592b4cb1b3
Suhan Wijaya <suhanw@gmail.com>
2019-01-07 23:36:55 -0500

    cleanup

--- src/server/sockets/chat_namespace.js
+++ src/server/sockets/chat_namespace.js
@@ -4,16 +4,15 @@ const chatNamespace = function (io) {
     const { chatRoom, currentUsername, activeChatPartner } = socket.handshake.query;
     socket.username = currentUsername;
 
-
     socket.join(chatRoom, () => {
       // 1. Check if the chat partner is currently online
       // 1a. let the partner know that I'm online by emitting event to the client-side
       socket.to(chatRoom).emit(`${socket.username} online`);
       // 1b. check if the partner is online
-      chatNsp.to(chatRoom).clients((err, clients) => { // identify clients (arr of socket Ids) connected to the chatRoom
+      chatNsp.to(chatRoom).clients((err, socketIds) => { // identify clients (arr of socket Ids) connected to the chatRoom
         if (err) throw err;
-        let onlineChatPartner = clients.find((client) => { // check if chat partner is in the chatRoom
-          let otherSocket = chatNsp.connected[client]; // chatNsp.connected is a hash of socket objects
+        let onlineChatPartner = socketIds.find((socketId) => { // check if chat partner is in the chatRoom
+          let otherSocket = chatNsp.connected[socketId]; // chatNsp.connected is a hash of socket objects
           return otherSocket.username === activeChatPartner;
         });
         if (onlineChatPartner) chatNsp.to(chatRoom).emit(`${activeChatPartner} online`); // if chat partner is online, emit event to client side
